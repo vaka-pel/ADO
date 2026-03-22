2b010279da6b8389d27c2adbe83ace9975d8e6fd
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-03-22 19:13:02 +0300

    Insert in progress

---
+++ ADO/Connector.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+	internal class Connector
+	{
+		string connection_string;
+		SqlConnection connection;
+
+		public Connector(string connection_string)
+		{
+			Console.WriteLine(connection_string);
+			this.connection_string = connection_string;
+			connection = new SqlConnection(connection_string);
+
+		}
+		public void Select(string cmd)
+		{
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+				Console.Write(reader.GetName(i) + "\t");
+			Console.WriteLine();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write($"{reader[i]}\t\t");
+				Console.WriteLine();
+			}
+			reader.Close();
+			connection.Close();
+		}
+		public void Select(string fields, string tables, string condition = "")
+		{
+			string cmd = $"SELECT {fields} FROM {tables}";
+			if (condition != "") cmd += $" WHERE {condition}";
+			cmd += ";";
+			Select(cmd);
+		}
+		public object Scalar(string cmd)
+		{
+			object result = null;
+			connection.Open();
+
+			SqlCommand command = new SqlCommand(cmd, connection);
+			result = command.ExecuteScalar(); //выполнение скалярного запроса
+
+			connection.Close();
+			return result;
+		}
+		public int GetMaxPrimaryKey(string table)
+		{
+			int PK = 0;
+			string cmd = $"SELECT * FROM {table}";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			string pk_name = reader.GetName(0);
+			reader.Close();
+			connection.Close();
+			return (int) Scalar($"SELECT MAX({pk_name}) FROM {table}");
+
+		}
+		public int GetNextPrimaryKey(string table)
+		{ 
+			return GetMaxPrimaryKey(table) + 1;
+		}
+		public void Insert(string cmd)
+		{
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			try
+			{
+				command.ExecuteNonQuery();
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.GetType());
+				Console.WriteLine(ex.Message);
+				if (ex.GetType() == typeof(SqlException) && ex.Message.Contains("_id"))
+				{ 
+					Console.WriteLine("Good");
+				}
+			}
+			connection.Close();
+		}
+		
+	}
+}
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -4,42 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using ADO;
 
 namespace PV_521_ADO
 {
 	internal class Program
 	{
+		
 		static void Main(string[] args)
 		{
 			//строка подключения
 			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-			Console.WriteLine(connection_string);
+			Connector connector = new Connector(connection_string);
 
-			SqlConnection connection = new SqlConnection(connection_string);
-			connection.Open();
+			connector.Insert("INSERT Directors (first_name,last_name) VALUES (N'Guy', N'Richie'); ");
 
-			string cmd = 
-				"SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-			SqlCommand command = new SqlCommand(cmd, connection);
+			Console.WriteLine($"PK MAX:\t{connector.GetMaxPrimaryKey("Directors")}");
+			
+			//string cmd = 
+			//	"SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
 
-			SqlDataReader reader = command.ExecuteReader();
-			for (int i = 0; i < reader.FieldCount; i++)
-				Console.Write(reader.GetName(i) + "\t");
-			Console.WriteLine();
-			while (reader.Read())
-			{
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-				for (int i = 0; i < reader.FieldCount; i++)
-					Console.Write($"{reader[i]}\t\t");
-				Console.WriteLine();
-			}
-			reader.Close();
+			//connector.Select(cmd);
+			connector.Select("*", "Directors");
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
 
-			command.CommandText = "SELECT COUNT(*) FROM Movies";
-			Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}") ; // Execute Scalar вынимает одно значение
+			connector.Select
+				(
+				"title,release_date,first_name,last_name",
+				"Movies,Directors",
+				"director=director_id"
+				);
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
 
-			connection.Close();
+
+			//command.CommandText = "SELECT COUNT(*) FROM Movies";
+			//Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}") ; // Execute Scalar вынимает одно значение
+
+			//connection.Close();
 		}
+		
 	}
 }
