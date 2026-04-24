c703c058b67b8a80999f794ac73797528944cc36
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-04-24 21:35:02 +0300

    AcademyWPF START

---
+++ AcademyWPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using DBTools;
+
+namespace AcademyWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Connector connector;
+		DataGrid[] tables;
+		public MainWindow()
+		{
+			InitializeComponent();
+			connector = new Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
+			tables = new DataGrid[] { dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers };
+		}
+
+		private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			int i = (sender as TabControl).SelectedIndex;
+			tables[i].ItemsSource = connector.
+				Select($"SELECT * FROM {((sender as TabControl).Items[i] as TabItem).Header.ToString()}").DefaultView;
+			statusBarCount.Text = $"Количество записей: {tables[i].Items.Count - 1}";
+		}
+	}
+}
---
+++ AcademyWPF/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyWPF
+{
+	class Query
+	{
+		public string Fields { get; set; }
+		public string Tables { get; set; }
+		public string Condition { get; set; }
+		public Query(string fields, string tables, string condition = "")
+		{
+			Fields = fields;
+			Tables = tables;
+			Condition = condition;
+		}
+		public override string ToString()
+		{
+			string query = $"SELECT {Fields} FROM {Tables}";
+			if (Condition != "") query += $" WHERE {Condition}";
+			return query;
+		}
+	}
+}
